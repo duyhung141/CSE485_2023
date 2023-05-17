fac62d85315cb773674e592fb3323056713ffbbc
thanhthao77 <tranthanhthao772k2@gmail.com>
2023-05-17 22:18:27 +0700

    Connect Database

--- BTTH02/Models/ConnectDatabase.php
+++ BTTH02/Models/ConnectDatabase.php
@@ -1,13 +1,15 @@
 <?php
 
 
+use Models\Student;
+
 class ConnectDatabase
 {
     private $host = 'localhost';
     private $dbname = 'cse485';
     private $username = 'root';
     private $password = '';
-
+    private $conn;
     public function __construct()
     {
         try {
@@ -15,6 +17,23 @@ class ConnectDatabase
         } catch (Exception $e) {
             die("Could not connect to the database $this->dbname :" . $e->getMessage());
         }
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
+
+    public function executeQuery($sql){
+        $dbConn = new ConnectDatabase();
+        $conn = $dbConn->getConnection();
+        $stmt = $conn->query($sql);
+
+        $students = [];
+        while ($row = $stmt->fetch()){
+            $student = new Student($row['id'], $row['name'], $row['birth'], $row['contact'], $row['user_id']);
+            array_push($students, $student);
+        }
+        return $students;
     }
 }
 ?>

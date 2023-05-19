1e91bd5d614774715e878f6685178f23ee938a49
thanhthao77 <tranthanhthao772k2@gmail.com>
2023-05-19 19:50:06 +0700

    fix aleart Email or password does not exist for Login

--- BTTH02/Controllers/loginController.php
+++ BTTH02/Controllers/loginController.php
@@ -4,12 +4,10 @@ class LoginController{
     public function login(){
         $pdo = new ConnectDatabase();
         if(isset($_POST['btnLogin'])){
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-
+            $email = trim($_POST['email']);
+            $password = trim($_POST['password']);
             try {
                 $conn = $pdo->getConnection();
-
                 if(empty($email) || empty($password)){
                     echo "<script>alert('Please enter your email and pasword');</script>";
                     exit();
@@ -24,6 +22,8 @@ class LoginController{
                     $_SESSION['user_id'] = $user['id'];
                     $_SESSION['user_email'] = $user['email'];
                     header("location:dashboard.php");
+                } else{
+                    echo "<script>alert('Email or password does not exist');</script>";
                 }
             } catch(Exception $e){
                 echo 'Error: '.$e->getMessage();

3d065c18ff38786df7bef13f1328a2dad5446e12
QuocHung <dauhuyen1@gmail.com>
2023-05-20 12:25:14 +0700

    error.php

---
+++ BTTH02/Views/error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body style="background-color: lightgray; font-family: Roboto; display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+    <h1 style="color: #1D1D75; font-size: 100px; text-align: center;">404 error</h1>
+    <p style="color: #404040; font-size: 40px; text-align: center;">This page doesn't exist</p>
+    <p style="color: #404040; font-size: 40px; text-align: center;">Would you like to <a href="https://chat.openai.com/" style="color: coral;">learn about HTTP errors</a>?</p>
+</body>
+</html>
